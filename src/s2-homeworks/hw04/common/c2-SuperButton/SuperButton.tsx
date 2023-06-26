7e6fd6c1d0dafa1a1820b78c4e6f9f539f14664c
Ilya Esenko <ilya.esenko@gmail.com>
2023-06-26 20:08:03 +0300

    update homework4, button and checkbox

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,8 +17,8 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button + (xType === 'red'  ? ' ' + `${s.red}` : (xType === 'red' && disabled === true) ? ' ' + `${s.disabled}` :
-        xType === 'secondary' ? ' ' + `${s.secondary}` : ' ' + `${s.default}`)
+    const finalClassName = s.button + (disabled === true ? ' ' + s.disabled : xType === 'red'  ? ' ' + s.red :
+        xType === 'secondary' ? ' ' + s.secondary : ' ' + s.default)
 
     // {`${s.button} + ${s.red}`} +(className ? ' ' + className : '') // задачка на смешивание классов
 
