476d6adec5ec6482c69c19eb8f5b4e7e3dd7324f
Ilya Esenko <ilya.esenko@gmail.com>
2023-06-26 19:36:10 +0300

    add homework4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,10 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    const finalClassName = s.button + (xType === 'red'  ? ' ' + `${s.red}` : (xType === 'red' && disabled === true) ? ' ' + `${s.disabled}` :
+        xType === 'secondary' ? ' ' + `${s.secondary}` : ' ' + `${s.default}`)
+
+    // {`${s.button} + ${s.red}`} +(className ? ' ' + className : '') // задачка на смешивание классов
 
     return (
         <button
